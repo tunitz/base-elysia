bfea318cc944fe51964d0c88af72dbd37eb26bbf
Lipton Abillar <liptonabillar@gmail.com>
2023-10-15 01:28:39 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .get("/", () => "Hello Elysia")
+    .listen({
+        port: process.env.PORT,
+        hostname: process.env.HOST
+    });
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
